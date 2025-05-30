422076b525bcfb46c904cb727fb98688c09e727c
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-05-30 11:38:23 +0600

    Removed unnecessary packages.

--- src/db/clients/mongo.client.ts
+++ src/db/clients/mongo.client.ts
@@ -1,19 +1,19 @@
-// db.ts
-import mongoose from 'mongoose';
-import { getEnvVar } from '../../utils/common.utils';
+// // db.ts
+// import mongoose from 'mongoose';
+// import { getEnvVar } from '../../utils/common.utils';
 
-const CONTENT_MONGO_URI = getEnvVar('CONTENT_MONGO_URI');
+// const CONTENT_MONGO_URI = getEnvVar('CONTENT_MONGO_URI');
 
-export function connectMongoose() {
-  mongoose.connect(CONTENT_MONGO_URI, {});
+// export function connectMongoose() {
+//   mongoose.connect(CONTENT_MONGO_URI, {});
 
-  const db = mongoose.connection;
+//   const db = mongoose.connection;
 
-  db.on('error', (err) => {
-    console.error(err);
-  });
+//   db.on('error', (err) => {
+//     console.error(err);
+//   });
 
-  db.once('open', () => {
-    console.log('Connected to MongoDB');
-  });
-}
+//   db.once('open', () => {
+//     console.log('Connected to MongoDB');
+//   });
+// }
